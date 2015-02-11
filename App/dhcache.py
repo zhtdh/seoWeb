d34044ebbb07e67ddf0d108abbdbbd3854fd60a7
zhangtaoqd <zhangtao@penavicogw.com>
2015-02-11 13:41:46 +0800

    取公司简介

--- App/dhcache.py
+++ App/dhcache.py
@@ -1,20 +1,24 @@
 __author__ = 'blaczom'
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseRedirect
 from django.views.decorators.http import *
 
+from App.viewsFore import getCorpInfo
 @require_http_methods(["GET"])
 def gool(request, goolArg="", goolSecArg=""):
-  if goolArg == "resec" :
-    return render(request, "rendSec.html")
-  elif goolArg == "rethi" :
-    return render(request, "rendThi.html")
-  elif goolArg == "refou" :
-    return render(request, "rendFou.html")
-  elif goolArg == "refiv" :
-    return render(request, "rendFiv.html", locals())
-  elif goolArg == "recon" :
-    return render(request, "rendCon.html")
-  else:
-    return HttpResponseRedirect("/")
-
+    try:
+        if goolArg == "resec":
+            corpInfo = getCorpInfo()
+            return render(request, "rendSec.html",locals())
+        elif goolArg == "rethi":
+            return render(request, "rendThi.html")
+        elif goolArg == "refou":
+            return render(request, "rendFou.html")
+        elif goolArg == "refiv":
+            return render(request, "rendFiv.html", locals())
+        elif goolArg == "recon":
+            return render(request, "rendCon.html")
+        else:
+            return HttpResponseRedirect("/")
+    except Exception as e:
+        log("gool执行错误：%s" % str(e.args))
