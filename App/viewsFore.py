c26d7fec06680312da340033629c94aaecad5fae
zhangtaoqd <zhangtao@penavicogw.com>
2015-02-11 13:28:48 +0800

    取公司简介

--- App/viewsFore.py
+++ App/viewsFore.py
@@ -1,8 +1,12 @@
 __author__ = 'blaczom'
 from django.shortcuts import render,HttpResponse
-
+from App.models import Article
 
 def home(request):
-
     return render(request, "home.html")
-
+def getCorpInfo():
+    try:
+        return Article.objects.filter(parent_id='0')\
+            .filter(kind__icontains='corpinfo').values('content')[0]['content']
+    except IndexError:
+        return "无简介"
